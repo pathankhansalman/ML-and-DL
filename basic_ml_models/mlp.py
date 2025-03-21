155a2f59c7c6c5f6375126bb5966cc46095e8897
Pathan Salman Khan <pathankhan.salman@gmail.com>
2025-03-21 14:00:00 +0000

    [March] MLP implementation with forward and backward propagate

--- basic_ml_models/mlp.py
+++ basic_ml_models/mlp.py
@@ -1,3 +1,13 @@
+import numpy as np
+
 class MLP:
-    def __init__(self):
-        pass
+    def __init__(self, input_size, hidden_size, output_size):
+        self.w1 = np.random.randn(input_size, hidden_size) * 0.01
+        self.b1 = np.zeros((1, hidden_size))
+        self.w2 = np.random.randn(hidden_size, output_size) * 0.01
+        self.b2 = np.zeros((1, output_size))
+    def forward(self, X):
+        self.z1 = np.dot(X, self.w1) + self.b1
+        self.a1 = np.tanh(self.z1)
+        self.z2 = np.dot(self.a1, self.w2) + self.b2
+        return self.z2
