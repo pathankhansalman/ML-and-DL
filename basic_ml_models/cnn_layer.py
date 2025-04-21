b2896d3272ff609c181e5cb73e0733acd17b7b8e
Pathan Salman Khan <pathankhan.salman@gmail.com>
2025-04-21 14:00:00 +0100

    [April] CNN layer PyTorch convolution logic

--- basic_ml_models/cnn_layer.py
+++ basic_ml_models/cnn_layer.py
@@ -1,3 +1,9 @@
-class CNNLayer:
-    def __init__(self):
-        pass
+import torch
+import torch.nn as nn
+
+class CNNLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size)
+    def forward(self, x):
+        return self.conv(x)
