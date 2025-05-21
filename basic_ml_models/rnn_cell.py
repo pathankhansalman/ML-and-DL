9906a24d0fd6e1c6d9e5b957c9e2cf71515fb61e
Pathan Salman Khan <pathankhan.salman@gmail.com>
2025-05-21 14:00:00 +0100

    [May] RNN Cell step computation logic

--- basic_ml_models/rnn_cell.py
+++ basic_ml_models/rnn_cell.py
@@ -1,3 +1,10 @@
-class RNNCell:
-    def __init__(self):
-        pass
+import torch
+import torch.nn as nn
+
+class RNNCell(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.xh = nn.Linear(input_size, hidden_size)
+        self.hh = nn.Linear(hidden_size, hidden_size)
+    def forward(self, x, h):
+        return torch.tanh(self.xh(x) + self.hh(h))
