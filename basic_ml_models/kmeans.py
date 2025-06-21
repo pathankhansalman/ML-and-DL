e221d13b762866aed40c8722e9ffdbd223fb36f0
Pathan Salman Khan <pathankhan.salman@gmail.com>
2025-06-21 14:00:00 +0100

    [June] KMeans NumPy fit algorithm

--- basic_ml_models/kmeans.py
+++ basic_ml_models/kmeans.py
@@ -1,3 +1,15 @@
+import numpy as np
+
 class KMeans:
-    def __init__(self):
-        pass
+    def __init__(self, k=3, max_iters=100):
+        self.k = k
+        self.max_iters = max_iters
+    def fit(self, X):
+        self.centroids = X[np.random.choice(X.shape[0], self.k, replace=False)]
+        for _ in range(self.max_iters):
+            distances = np.linalg.norm(X[:, np.newaxis] - self.centroids, axis=2)
+            labels = np.argmin(distances, axis=1)
+            new_centroids = np.array([X[labels == i].mean(axis=0) for i in range(self.k)])
+            if np.allclose(self.centroids, new_centroids): break
+            self.centroids = new_centroids
+        return labels
